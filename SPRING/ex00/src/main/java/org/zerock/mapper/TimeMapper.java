a81131488f0e92249c6290e9aecaf17647603a6a
kimrumm <kimrumm@naver.com>
2021-08-10 01:53:47 +0900

    Spring Part01

--- SPRING/ex00/src/main/java/org/zerock/mapper/TimeMapper.java
+++ SPRING/ex00/src/main/java/org/zerock/mapper/TimeMapper.java
@@ -1,6 +1,7 @@
 package org.zerock.mapper;
 
 import org.apache.ibatis.annotations.Select;
+//org.zerock.mapper.TimeMapper.getTime2 -> 오리지널 경로
 
 //시간을 가지고 테스트 하는 습관 가지기
 public interface TimeMapper {
@@ -8,5 +9,7 @@ public interface TimeMapper {
     //mybatis 주의사항 ! -> ; 없어야 한다.
     @Select("select sysdate from dual")
     String getTime ();
+
+    String getTime2();
 
 }
