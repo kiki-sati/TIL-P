cc444bcddbf2088a9f34fe71268974bb07f8b697
Kim Arum <83346234+kimrumm@users.noreply.github.com>
2022-09-21 15:54:26 +0900

    Feat : modern java in action

---
+++ JAVA/modern-java-in-action/modern-java-in-action/src/main/java/com/example/morderjavainaction/MorderJavaInActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.morderjavainaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorderJavaInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorderJavaInActionApplication.class, args);
+	}
+
+}
---
+++ JAVA/modern-java-in-action/modern-java-in-action/src/test/java/com/example/morderjavainaction/MorderJavaInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.morderjavainaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorderJavaInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
