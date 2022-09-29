f972a557f9198d7275e0a488faa4f48415e6210b
Kim Arum <83346234+kimrumm@users.noreply.github.com>
2022-09-29 16:56:57 +0900

    Feat: 상품 엔티티 개발(비지니스 로직 추가)

--- JPA/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ JPA/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -10,19 +10,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
 
 	@Id
@@ -37,5 +34,27 @@ public abstract class Item {
 	@ManyToMany(mappedBy = "items")
 	private List<Category> categories = new ArrayList<>();
 
+	//==비지니스 로직(응집도-객체지향적)==//
+	// 데이터를 가지고 있는 쪽에 비지니스 로직이 있는게 관리하기 좋다.
+	// 핵심 비지니스 로직을 엔티티에 직접 구현
+	// 데이터 변경을 하려면 setter 로 변경하는게 아니라 핵심 비지니스 로직으로 변경하는 것.
+
+	/**
+	 * stock 증가
+	 */
+	public void addStock(int quantity) {
+		this.stockQuantity += quantity;
+	}
+
+	/**
+	 * stock 감소
+	 */
+	public void removeStock(int quantity) {
+		int restStock = this.stockQuantity - quantity;
+		if (restStock < 0) {
+			throw new NotEnoughStockException("need more stock");
+		}
+		this.stockQuantity = restStock;
+	}
 
 }
